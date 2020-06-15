edf42b3a2ded76457e53e1537139625b6fa6540d
Wq <1821478380@qq.com>
2020-06-15 10:25:51 +0800

    解决移动端 300ms延迟

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Fastclick from"fastclick";
+
+
 Vue.config.productionTip = false
 
+
+// 解决移动端 300ms延迟
+Fastclick.attach(document.body)
 new Vue({
   router,
   store,
